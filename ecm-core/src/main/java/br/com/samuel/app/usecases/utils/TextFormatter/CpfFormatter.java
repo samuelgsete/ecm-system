cc22987ea4d69e69f8ec06c357d899c79e0fbe0d
samuel <samuelgsete@gmail.com>
2023-06-18 12:22:22 -0300

    tratamento de dados nulo no template da credential

--- ecm-core/src/main/java/br/com/samuel/app/usecases/utils/TextFormatter/CpfFormatter.java
+++ ecm-core/src/main/java/br/com/samuel/app/usecases/utils/TextFormatter/CpfFormatter.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CpfFormatter {
     public String run(String cpf) {
+        if(cpf == null) return "************";
         if(cpf.length() != 11) return cpf;
         var result = cpf.substring(0,3) + "." + cpf.substring(3,6) + "." + cpf.substring(6,9) + "-" + cpf.substring(9,11);
         return result;
